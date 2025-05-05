43cdc3a74910899082d74d920c8a3233e74b0621
Mr. Python <hello-python@outlook.com>
2025-05-05 10:25:22 +0800

    #添加IconFont

---
+++ Round.NET.AvaloniaApp.MinecraftLauncher/Views/Controls/FontIcon.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Round.NET.AvaloniaApp.MinecraftLauncher.Views.Controls;
+
+public partial class FontIcon : UserControl
+{
+    public FontIcon()
+    {
+        InitializeComponent();
+    }
+}
