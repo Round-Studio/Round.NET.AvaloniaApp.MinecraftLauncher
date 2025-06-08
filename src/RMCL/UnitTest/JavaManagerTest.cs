e1c538159bb620e5c166419fe51eade49c7fb8db
DOMI <YoumiHa@dingtalk.com>
2025-06-08 21:35:53 +0800

    feat: add javasearch support (Windows)

---
+++ src/RMCL/UnitTest/JavaManagerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RMCL.JavaManager;
+
+namespace UnitTest
+{
+    public class JavaManagerTest
+    {
+        [Test]
+        public void Test()
+        {
+            var javaDetilsArray = JavaManager.SearchJavaAsync().Result;
+        }
+    }
+}
