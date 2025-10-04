c28bf3e5979722d0e991e5fad6bb6157b9fe5e76
Minecraft一角钱 <liy_0924@qq.com>
2025-10-04 13:30:39 +0800

    feat: 插件设置页面

--- src/Plugin.BedrockBoot/Views/Pages/MainHomePage.axaml.cs
+++ src/Plugin.BedrockBoot/Views/Pages/MainHomePage.axaml.cs
@@ -1,13 +1,33 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Plugin.BedrockBoot.Views.Pages.MainSubPage;
 
 namespace Plugin.BedrockBoot.Views.Pages;
 
 public partial class MainHomePage : UserControl
 {
+    public bool IsEdit = false;
+
     public MainHomePage()
     {
         InitializeComponent();
+
+        IsEdit = true;
+    }
+
+    private void SelectingItemsControl_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        if (IsEdit)
+        {
+            var tag = this.Naviga.SelectedIndex;
+
+            switch (tag)
+            {
+                case 2:
+                    this.Frame.NavigateTo(new SettingsPage());
+                    break;
+            }
+        }
     }
 }
---
+++ src/Plugin.BedrockBoot/Views/Pages/MainSubPage/SettingsPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Plugin.BedrockBoot.Views.Pages.MainSubPage;
+
+public partial class SettingsPage : UserControl
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}
