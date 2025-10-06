11840dc5f4593159fc4a21f9760e0c4ef3eb04e8
Dime <liy_0924@qq.com>
2025-10-06 17:11:49 +0800

    feat: 修复个性化页面鸟瞰图不显示背景

--- src/RMCL/Views/Control/StyleBox.axaml.cs
+++ src/RMCL/Views/Control/StyleBox.axaml.cs
@@ -2,8 +2,10 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using RMCL.Base.Enum.Style;
 using RMCL.Models;
 using RMCL.Models.Global;
 
@@ -19,6 +21,10 @@ public partial class StyleBox : UserControl
 
     public void UpdateBack()
     {
+        RightImage.IsVisible = false;
+        LeftImage.IsVisible = false;
+        BackgroundBox.IsVisible = false;
+        
         var name = BackMaterialHelper.GetStringName(GlobalModels.Config.Data.StyleConfig.BackMaterialType);
 
         if (!string.IsNullOrEmpty(name))
@@ -36,7 +42,30 @@ public partial class StyleBox : UserControl
                     // 4. 现在你可以将 bitmap 赋值给 Image 控件的 Source 属性
                     RightImage.Source = bitmap;
                     LeftImage.Source = bitmap;
+                    
+                    RightImage.IsVisible = true;
+                    LeftImage.IsVisible = true;
                 }
+            }
+        }
+
+        if (GlobalModels.Config.Data.StyleConfig.StyleType == StyleType.Image)
+        {
+            BackgroundBox.IsVisible = true;
+
+            using (var originalBitmap = new Bitmap(GlobalModels.Config.Data.StyleConfig.BackgroundImages[
+                       GlobalModels.Config.Data.StyleConfig.BackgroundImageSelectedIndex]))
+            {
+                // 直接基于高度120计算宽度
+                int newWidth = (int)(120 * ((double)originalBitmap.Size.Width / originalBitmap.Size.Height));
+
+                var resizedBitmap = originalBitmap.CreateScaledBitmap(new PixelSize(newWidth, 120));
+
+                BackgroundBox.Background = new ImageBrush()
+                {
+                    Stretch = Stretch.UniformToFill,
+                    Source = resizedBitmap
+                };
             }
         }
     }
