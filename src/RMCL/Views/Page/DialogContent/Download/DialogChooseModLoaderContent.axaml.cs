94528df1ccca15eff2376f8a06d13d05c7e1f9c6
Dime <liy_0924@qq.com>
2025-10-19 16:42:35 +0800

    feat: 选择卡片

---
+++ src/RMCL/Views/Page/DialogContent/Download/DialogChooseModLoaderContent.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace RMCL.Views.Page.DialogContent.Download;
+
+public partial class DialogChooseModLoaderContent : UserControl
+{
+    public DialogChooseModLoaderContent()
+    {
+        InitializeComponent();
+    }
+}
--- src/RMCL/Views/Page/Main/MainSubPage/MainDownloadPage.axaml.cs
+++ src/RMCL/Views/Page/Main/MainSubPage/MainDownloadPage.axaml.cs
@@ -1,13 +1,22 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using RMCL.Views.Page.Main.DownloadPage;
 
 namespace RMCL.Views.Page.Main.MainSubPage;
 
 public partial class MainDownloadPage : UserControl
 {
+    public static MainDownloadPage? Instance { get; private set; }
     public MainDownloadPage()
     {
+        Instance = this;
         InitializeComponent();
+        
+        BreadcrumbBar.RootItemClick = () =>
+        {
+            DownloadFrame.NavigateTo(new DownloadRecommend());
+        };
+        DownloadFrame.NavigateTo(new DownloadRecommend());
     }
 }
