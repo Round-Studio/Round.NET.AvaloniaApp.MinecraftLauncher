5c465915369e02513768fd4041bfbbbe457af142
Dime <liy_0924@qq.com>
2025-12-06 08:30:05 +0800

    feat: 将 PluginTools 移动至 SDK

--- buildConfig/buildPluginAndDebug.bat
+++ buildConfig/buildPluginAndDebug.bat
@@ -1,6 +1,6 @@
 @echo off
 
 md "%APPDATA%\RoundStudio\RMCL4\RMCL.Plugin"
-dotnet publish ./src/PluginTools/PluginTools.csproj -c Release -o ./build/PluginTools
+dotnet publish ./src/Round.SDK/PluginTools/PluginTools.csproj -c Release -o ./build/PluginTools
 powershell ./build/PluginTools/PluginTools.exe -b -config ./buildConfig/Plugin.MusicPlayer.json
 powershell copy "build/Plugin.BedrockBoot/publish/pack.rplck" "%APPDATA%\RoundStudio\RMCL4\RMCL.Plugin\BedrockBoot.rplck"
